51e96a42e5c492b1967478123c9cea259cc46230
Danil0990 <wpigyn@gmail.com>
2022-03-12 13:53:01 +0500

    0,5

--- Assets/Skripts/HealthBar.cs
+++ Assets/Skripts/HealthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,49 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private float _power;
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _minHealth;
     [SerializeField] private Slider _slider;
-    [SerializeField] private Player _player;
+
+    private float _health;
 
     private void Start()
     {
-        _slider.maxValue = _player.Health;
+        _health = _maxHealth;
     }
 
-    private void FixedUpdate()
+    private IEnumerator SliderValueChnge()
     {
-        _slider.value = Mathf.MoveTowards(_slider.value, _player.Health, 10f * Time.deltaTime);
+        while (_slider.value != _health)
+        {
+        _slider.value = Mathf.MoveTowards(_slider.value, _health, 10f * Time.deltaTime);
+        yield return null;
+        }
+        StopCoroutine(SliderValueChnge());
+    }
+
+    private void CoroutineStart()
+    {
+        var coroutineJob = StartCoroutine(SliderValueChnge());
+    }
+
+   
+    public void TakeHeal()
+    {
+        if (_health < _maxHealth)
+        {
+            _health += _power;
+        }
+        CoroutineStart();
+    }
+
+    public void TakeDamage()
+    {
+        if (_health > _minHealth)
+        {
+            _health -= _power;
+        }
+        CoroutineStart();
     }
 }
